a591899ea7750f8afc26f2e46a50e2c1a4f40da4
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2026-06-16 19:14:25 +0530

    1.java

--- 1.java
+++ 1.java
@@ -1,5 +1,56 @@
+/*
 class FirtsClass{
     public static void main(String args[]){
         System.out.println("Hello World!!");
     }
 }
+*/
+
+/*
+public class Main{
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        int diff=a-b;
+        System.out.println(sum);
+        System.out.println(diff);
+    }
+}
+*/
+
+//NAME
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String name= sc.next();
+        System.out.println(name);
+    }
+}
+*/
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String name= sc.nextLine();  //next will pick only one word but nextLine will pick full string
+        System.out.println(name);
+        
+    }
+}
+*/
+//Take 2 variables a and b and print their sum
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
+*/
