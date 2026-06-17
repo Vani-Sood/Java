1da602155ea8fbb5cdd2c45d4dd74e5f790e8552
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2026-06-17 19:18:43 +0530

    2.java

---
+++ 2.java
@@ -0,0 +1,53 @@
+/*
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int age = sc.nextInt();
+      if(age>=18){
+          System.out.println("Adult");
+      }
+      else{
+          System.out.println("Child");
+      }
+  }
+}
+*/
+//odd or even
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int number= sc.nextInt();
+        if(number%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
+*/
+//to check if a, b which is greater
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+            int a= sc.nextInt();
+            int b= sc.nextInt();
+           
+            if(a>b){
+                System.out.println("a is greater");
+            }
+            else if(b>a){
+                System.out.println("B is greater");
+            }
+            else{
+                System.out.println("both are equal");
+            }       
+        
+    }
+}
+*/
