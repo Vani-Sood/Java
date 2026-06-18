fc0fd18dbbfde48cbccab24899b500a57c80df56
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2026-06-18 12:59:05 +0530

    3.java

---
+++ 3.java
@@ -0,0 +1,68 @@
+//to print hello world 3 times
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        for (int i=1; i<=3; i++){
+            System.out.println("Hello World!!!");
+        }
+    }
+}
+*/
+//to print no. 0-10
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
+*/
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int i=1;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+}
+*/
+//Sum of 1st n natural numbers
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+            int no=sc.nextInt();
+            
+            int sum=0;
+            for(int i=1; i<=no; i++){
+                sum+=i;
+                
+            }
+        System.out.println(sum);
+    }
+}
+*/
+//table of n
+/*
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int table=1;
+        for(int i=0;i<=10; i++){
+            table=i*n;
+            System.out.println(table);
+        }
+        
+    }
+}
+*/
